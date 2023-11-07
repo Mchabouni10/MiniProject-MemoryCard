d5986d2bbdd0fc9b0751d0fa2f3e9bc55dcf4408
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-11-07 09:53:12 -0600

    fifth commit

--- script.js
+++ script.js
@@ -28,17 +28,32 @@ let timerInterval;
 
 console.log(MyCards);
 
-// //let create function that randomize my array of cards this way we can change the postion of the cards each time revealed.
+
+//let create function that randomize my array of cards this way we can change the postion of the cards each time revealed.
 // const shuffle = (array) =>{
 //     for (let i = array.length - 1; i > 0; i--) { //starting from last element working down it garantee
 //         //the last element in our array have a chance of being swapped with any other element in our array
-//       const j = Math.floor(Math.random() * (i + 1));
-//       [array[i], array[j]] = [array[j], array[i]]; //generate a random integer j between 0 and i
+//       const j = Math.floor(Math.random() * (i + 1))
+//       [array[i], array[j]] = [array[j], array[i]] //generate a random integer j between 0 and i
 //       //using mathfloor to get a random integer with this range
 //       //swapping a value between two variables without a need of temporary variable.
 //     }
 
-//   };
+//   }
+
+//   shuffle(MyCards) //call function each time to suffle my card 
+
+
+  // Create my card on the grid using DOM like we learn it in class
+  // By using createElement and add a call to it by usin classList.add
+  for (let i=0; i < MyCards.length; i++){ // create a loop that iretrate all the lengrh of my array
+    const card = document.createElement('div')// creat a div for each element in my array
+    card.classList.add('card')// add a class name card for each card created 
+    card.dataset.MyCards = MyCards[i] //using dataset to import my card to my grid
+    console.log(MyCards)
+    card.innerText = "cards"
+    
+  }
 
 // //function that count the time by second ===============
 //   function startGameTimer() {
