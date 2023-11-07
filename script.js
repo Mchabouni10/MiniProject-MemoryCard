22ec2fb6cbd7bad3545813e891cf63f357f037e6
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-11-07 11:52:25 -0600

    7 commit

--- script.js
+++ script.js
@@ -29,35 +29,40 @@ let timerInterval;
 console.log(MyCards);
 
 
-//let create function that randomize my array of cards this way we can change the postion of the cards each time revealed.
-// const shuffle = (array) =>{
-//     for (let i = array.length - 1; i > 0; i--) { //starting from last element working down it garantee
-//         //the last element in our array have a chance of being swapped with any other element in our array
-//       const j = Math.floor(Math.random() * (i + 1))
-//       [array[i], array[j]] = [array[j], array[i]] //generate a random integer j between 0 and i
-//       //using mathfloor to get a random integer with this range
-//       //swapping a value between two variables without a need of temporary variable.
-//     }
+//let create function that randomize my array of cards this way we 
+//can change the postion of the cards each time revealed.
+const shuffleAndCreateGrid = (MyCards) => {
+    for (let i = MyCards.length - 1; i > 0; i--) {     //starting from last element working down it garantee
+      const j = Math.floor(Math.random() * (i + 1)); //the last element in our array have a chance of being swapped with any other element in our array
+      [MyCards[i], MyCards[j]] = [MyCards[j], MyCards[i]]; //generate a random integer j between 0 and i
+    }
 
-//   }
+    //using mathfloor to get a random integer with this range
+    //using mathfloor to get a random integer with this range
 
-//   shuffle(MyCards) //call function each time to suffle my card 
-
-
-  // Create my card on the grid using DOM like we learn it in class
-  // By using createElement and add a call to it by usin classList.add
-  const container = document.querySelector('.myScreen');
-  for (let i=0; i < MyCards.length; i++){ // create a loop that iretrate all the lengrh of my array
-    const card = document.createElement('div')// creat a div for each element in my array
-    card.classList.add('card')// add a class name card for each card created 
-    // card.setAttribute('class','card')
-
-    card.dataset.MyCards = MyCards[i] //using dataset to import my card to my grid
-    const img = document.createElement('img');
-    img.src = MyCards[i];
-    card.appendChild(img);
-    container.appendChild(card);
+    // Create my card on the grid using DOM like we learn it in class
+    const container = document.querySelector('.myScreen'); //access to my grid that I named myScreen
+    container.innerHTML = ''; //clean the screen each time
+  
+    for (let i = 0; i < MyCards.length; i++) {   // create a loop that iretrate all the lengrh of my array
+      const card = document.createElement('div'); // creat a div for each element in my array
+      card.classList.add('card'); // add a class name card for each card created
+      card.dataset.MyCards = MyCards[i]; //using dataset to import my card to my grid
+      
+      const img = document.createElement('img'); //create a variable img to add my images to card
+      img.src = MyCards[i]; //store each image inside my Array that I called Mycards
+      card.appendChild(img); //display my Image isnide the div
+      
+      container.appendChild(card); // display all cards on my Grid or Screen
+    }
   }
+  
+  // Call the function with your MyCards array
+  shuffleAndCreateGrid(MyCards);
+
+
+
+
 
 // //function that count the time by second ===============
 //   function startGameTimer() {
