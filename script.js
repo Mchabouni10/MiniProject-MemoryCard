93544d83f97be500bbb5feda26aa8ffae7430bc8
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-11-07 09:12:21 -0600

    third commit

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const cardGrid = document.getElementById('cards');
 const cardScore= document.getElementById('score');
 const cardTimer= document.getElementById('secondTimer');
 let selectedCards = [];
-let twoMatched = 0;
+let twoMatchedCards = 0;
 let waitForMove = 0;
-let gameStarted = false;
+let startThegame = false;
 let timerInterval;
 
 
 
 //let create function that randomize my array of cards this way we can change the postion of the cards each time revealed.
-const shuffle = ((array: element[]) => {   
+const shuffle = (array) =>{   
     for (let i = array.length - 1; i > 0; i--) { //starting from last element working down it garantee
         //the last element in our array have a chance of being swapped with any other element in our array
       const j = Math.floor(Math.random() * (i + 1)); 
@@ -20,18 +20,33 @@ const shuffle = ((array: element[]) => {
       //using mathfloor to get a random integer with this range
       //swapping a value between two variables without a need of temporary variable.
     } 
-  }); 
+
+  }; 
 
 
 
 //function that count the time by second ===============
-  function startTimer() {
+  function startGameTimer() {
     let sec = 0; //we starting from second zero
     timerInterval = setInterval(() => { //setInterval is built Javascript function that count 
         // repeadtly the specified function by fixed time on milliscond 
         sec++ ; // the incrementation of the seconds 
         cardTimer.textContent = seconds ; // this display the time in our screen.
     }, 1000); //setInterval will be excuted each 1000 miliisecond and is one second.
+}
+
+
+// Function to reaveal 2 card chosen
+function reaveal2chosenCard (card){ // function called reveal2chosenCards
+    //it called when the player click on the card
+    if (!startThegame) { // we set our variable startThegame to flase mean if there is click
+        startGameTimer() // The startGameTimer will be called to start counting the seconds 
+        startThegame = true //set the Variable startThegame to true to make sure that first card been clicked
+    }
+    
+
+
+
 }
 
 
