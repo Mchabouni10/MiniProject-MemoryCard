0d27537665c40d4ccf739a7d8d6dcf110c09ef08
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-11-08 10:31:50 -0600

    13 commit

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const MyCards = [
   "images/7.png",
   "images/8.png",
 ];
+
 const cardNumbers = MyCards.length;
 const cardGrid = document.getElementById("cards");
 const cardScore = document.getElementById("score");
@@ -25,68 +26,78 @@ let twoMatchedCards = 0;
 let waitForMove = 0;
 let startThegame = false;
 let timerInterval;
+let score = 0;
+let errorCount = 0;
 
-// console.log(MyCards);
 
-//let create function that randomize my array of cards this way we
-//can change the postion of the cards each time revealed.
-const shuffleAndCreateGrid = (MyCards) => {
-  for (let i = MyCards.length - 1; i > 0; i--) {
-    //starting from last element working down it garantee
-    const j = Math.floor(Math.random() * (i + 1)); //the last element in our array have a chance of being swapped with any other element in our array
-    [MyCards[i], MyCards[j]] = [MyCards[j], MyCards[i]]; //generate a random integer j between 0 and i
-  }
-
-  //using mathfloor to get a random integer with this range
-  //using mathfloor to get a random integer with this range
-
-  // Create my card on the grid using DOM like we learn it in class
-  const container = document.querySelector(".myScreen"); //access to my grid that I named myScreen
-  container.innerHTML = ""; //clean the screen each time
-
-  for (let i = 0; i < MyCards.length; i++) {
-    // create a loop that iretrate all the lengrh of my array
-    const card = document.createElement("div"); // creat a div for each element in my array
-    const backCard = document.createElement("div");
-
-    card.classList.add("card"); // add a class name card for each card created
-    backCard.classList.add("backCard");
-    card.dataset.MyCards = MyCards[i]; //using dataset to import my card to my grid
-
-    const img = document.createElement("img"); //create a variable img to add my images to card
-    img.src = MyCards[i]; //store each image inside my Array that I called Mycards
-    card.appendChild(img); //display my Image isnide the div
-    card.appendChild(backCard);
-
-    card.addEventListener("click", () => {
-        card.classList.toggle("turnOver");
-        backCard.classList.toggle('active')
-        reveal2chosenCard(card);
-        // if (!card.classList.contains("turnOver") && waitForMove < 2) {
-        //   card.classList.toggle("turnOver");
-        //   reveal2chosenCard(card);
-        // }
-      });
-
-    container.appendChild(card); // display all cards on my Grid or Screen
-  }
-};
-
-//========= Call the function with your MyCards array
 shuffleAndCreateGrid(MyCards);
+hideCards();
 
-//=========function that count the time by second ===============
-function startGameTimer() {
-  let seconds = 0; //we starting from second zero
-  timerInterval = setInterval(() => {
-    //setInterval is built Javascript function that count
-    // repeadtly the specified function by fixed time on milliscond
-    seconds++; // the incrementation of the seconds
-    cardTimer.textContent = seconds; // this display the time in our screen.
-  }, 1000); //setInterval will be excuted each 1000 miliisecond and is one second.
+// Function to shuffle an array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-//========= I need function to reset a cards hide front face
+// Function to create and shuffle the game grid
+function shuffleAndCreateGrid(cards) {
+  cardGrid.innerHTML = "";
+  shuffleArray(cards);
+
+  cards.forEach((card, index) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.dataset.index = index;
+
+    const cardFront = document.createElement("div");
+    cardFront.classList.add("card-front");
+
+    const cardBack = document.createElement("div");
+    cardBack.classList.add("card-back");
+
+    const img = document.createElement("img");
+    img.src = card;
+    cardFront.appendChild(img);
+
+    cardElement.appendChild(cardFront);
+    cardElement.appendChild(cardBack);
+
+    cardElement.addEventListener("click", () => {
+      if (!startThegame) {
+        startGameTimer();
+        startThegame = true;
+      }
+
+      if (waitForMove < 2 && !cardElement.classList.contains("matched")) {
+        cardElement.classList.toggle("turnOver");
+        reveal2chosenCard(cardElement);
+      }
+    });
+
+    cardGrid.appendChild(cardElement);
+  });
+}
+
+// Function to hide the cards (turn them face down)
+function hideCards() {
+  const allCards = document.querySelectorAll(".card");
+  allCards.forEach((card) => {
+    card.classList.remove("turnOver");
+  });
+}
+
+// Function to start the game timer
+function startGameTimer() {
+  let seconds = 0;
+  timerInterval = setInterval(() => {
+    seconds++;
+    cardTimer.textContent = seconds;
+  }, 1000);
+}
+
+// Function to reset the cards to their initial state (face down)
 function resetTheCards() {
   const allCards = document.querySelectorAll(".card");
   allCards.forEach((card) => {
@@ -94,92 +105,101 @@ function resetTheCards() {
   });
 }
 
-// Call the function to reset the cards when the game starts
-resetTheCards();
+// Function to reset the entire game
+function resetGame() {
+  clearInterval(timerInterval);
+  cardGrid.innerHTML = ""; // Clear the grid
+  cardTimer.textContent = "0";
+  score = 0;
+  errorCount = 0
+  twoMatchedCards = 0;
+  startThegame = false;
+  selectedCards = [];
+  waitForMove = 0;
+  resetTheCards(); // Reset the cards
+  shuffleAndCreateGrid(MyCards); // Create a new shuffled grid
+  hideCards(); // Hide the cards
+  updateScoreDisplay();
+  updateErrorCount()
+}
 
-
-
-// =========Function to reaveal 2 card chosen============
+// Function to reveal two chosen cards and handle the game logic
 function reveal2chosenCard(card) {
-  // function called reveal2chosenCards
-  //it called when the player click on the card
-  if (!startThegame) {
-    // we set our variable startThegame to flase mean if there is click
-    startGameTimer(); // The startGameTimer will be called to start counting the seconds
-    startThegame = true; //set the Variable startThegame to true to make sure that first card been clicked
-  }
-  if (waitForMove > 0) {
-    // Don't let the player select more than two cards
-    return;
-  }
+  if (waitForMove === 0) {
+    selectedCards.push(card);
 
-  // return my My clicked car
-  let MyClickedCards = [];
-  card.classList.add("turnOver");
-  MyClickedCards.push(card);
+    if (selectedCards.length === 2) {
+      waitForMove = 2;
 
-  if (MyClickedCards.length === 2) {
-    //if there is just 2 cards selected
-    waitForMove = 2; // waitForMove is set 2 to for preventing to compare more than 2 cards
-    const firstCard = MyClickedCards[0];
-    const secondCard = MyClickedCards[1];
+      const firstCard = selectedCards[0];
+      const secondCard = selectedCards[1];
 
-    // Compare the two selected cards
-    if (firstCard.dataset.MyCards === secondCard.dataset.MyCards) {
-      // statement to check or compare if both cards are matched
-      // Match found
+      if (firstCard.dataset.index === secondCard.dataset.index) {
+        selectedCards.pop();
+        waitForMove = 0;
+        return;
+      }
 
-      setTimeout(() => {
-        //give the affect to show that two card as matched and have the same image
-        // MyClickedCards.forEach((card) => { //iterate inside my Array to add the class matched for turning over again
+      if (
+        firstCard.querySelector("img").src ===
+        secondCard.querySelector("img").src
+      ) {
         firstCard.classList.add("matched");
         secondCard.classList.add("matched");
-        //   cardGrid.removeChild(card); //I need to check this==============
         cardGrid.removeChild(firstCard);
         cardGrid.removeChild(secondCard);
-        // });
 
-        MyClickedCards = []; //set my Array to empty again
-        twoMatchedCards += 2; //increment that 2 more matched cards found
-        waitForMove = 0; //set to 0 to let the player click again two different cards
+        selectedCards = [];
+        waitForMove = 0;
+        twoMatchedCards += 2;
+        score += 1; // Update player score
+        updateScoreDisplay(); // Update the score display on the page
+        
 
         if (twoMatchedCards === cardNumbers) {
-          //check if the player one won means our twoMatchedCard is 16
-          //and is the same number of cards
-          clearInterval(timerInterval); // stop the time when the player and start when restard the game again
-          setTimeout(
-            () =>
-              alert(
-                "Congratulations! You won! Your time: " + cardTimer.textContent
-              ),
-            500
-          );
+          clearInterval(timerInterval);
+          setTimeout(() => {
+            alert(
+              "Congratulations! You won! Your time: " + cardTimer.textContent
+            );
+          }, 500);
         }
-      }, 500);
-    } else {
-      //else statament if the 2 cards doesn
-
-      setTimeout(() => {
-        //use function setTimeout to two turnedOver cards
-        MyClickedCards.forEach((card) => {
-          //iterate my Array
-          card.classList.remove("turnOver"); //once turnover change the classlist to be checked again
-        });
-        MyClickedCards = []; // set My array to zero to get empty again to choose another cards
-        waitForMove = 0; //set move to zero to let the player can have 2 clicks again
-      }, 1000);
+      } else {
+        errorCount += 1
+        updateErrorCount()
+        setTimeout(() => {
+          firstCard.classList.remove("turnOver");
+          secondCard.classList.remove("turnOver");
+          selectedCards = [];
+          waitForMove = 0;
+        }, 1000);
+      }
     }
   }
 }
-//set up my clicks on cards using add event Listner
-// const allCards = document.querySelectorAll(".card");
-// allCards.forEach((card) => {
-//   card.addEventListener("click", () => {
-//     card.classList.toggle("turnOver");
-//     reveal2chosenCard(card);
-//     // if (!card.classList.contains("turnOver") && waitForMove < 2) {
-//     //   card.classList.toggle("turnOver");
-//     //   reveal2chosenCard(card);
-//     // }
-//   });
-// });
+
+const restartButton = document.getElementById("restartButton");
+restartButton.addEventListener("click", resetGame);
+
+// Initial game setup
+shuffleAndCreateGrid(MyCards);
+hideCards(); // Hide the cards when the game starts
+updateScoreDisplay();
+updateErrorCount();
+
+
+
+
+// function for my score ============================
+function updateScoreDisplay() {
+  const scoreDisplay = document.getElementById("score");
+  scoreDisplay.textContent = `Score: ${score}`;
+}
+
+
+
+//funct
+function updateErrorCount() {
+    const errorCountScreen = document.getElementById("errorCount");
+    errorCountScreen.textContent = `Errors: ${errorCount}`;
+  }
